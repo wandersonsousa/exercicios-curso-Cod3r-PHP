3c2f90250bfa214fb1f280e3f2942ffd82b01634
Wanderson3004 <wandersonsousa3004@gmail.com>
2020-08-02 10:07:45 -0300

    push code

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Curso PHP</title>
+</head>
+<body>
+
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Índice de Exercícios</h2>
+    </header>
+
+    <main class="principal">
+        <div class="conteudo">
+            <div class="modulos">
+                <div class="modulo verde">
+                    <h3>Módulo 01 - Básico</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                    </ul>       
+                </div>
+                <div class="modulo vermelho">
+                    <h3>Módulo 02 - Básico</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                    </ul>       
+                </div>
+                <div class="modulo azul">
+                    <h3>Módulo 03 - Básico</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                    </ul>       
+                </div>
+                <div class="modulo roxo">
+                    <h3>Módulo 04 - Básico</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                    </ul>       
+                </div>
+                <div class="modulo laranja">
+                    <h3>Módulo 05 - Básico</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                    </ul>       
+                </div>
+                <div class="modulo verde-escuro">
+                    <h3>Módulo 06 - Básico</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                    </ul>       
+                </div>
+
+                <div class="modulo vermelho-escuro">
+                    <h3>Módulo 07 - Básico</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                    </ul>       
+                </div>
+                <div class="modulo azul-escuro">
+                    <h3>Módulo 08 - Básico</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                    </ul>       
+                </div>
+                <div class="modulo roxo-escuro">
+                    <h3>Módulo 09 - Básico</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                    </ul>       
+                </div>
+                
+            </div>
+        </div>
+    </main>
+
+    <footer class="rodape">
+        wandersonsousa
+    </footer>
+
+    
+</body>
+</html>
