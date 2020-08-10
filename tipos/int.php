0340480649f4d84b9e2e52793d664b3de1b8c619
Wanderson3004 <wandersonsousa3004@gmail.com>
2020-08-10 11:24:29 -0300

    add type float in basicclass

---
+++ tipos/int.php
@@ -0,0 +1,18 @@
+<div class="titulo">Tipo Inteiro</div>
+
+<?php
+
+
+
+echo 11;
+echo "<br>";
+
+var_dump(11);
+echo "<br>";
+
+echo PHP_INT_MAX,"<br>";
+echo PHP_INT_MIN,"<br>";
+
+echo 011,"<br>"; //base octal
+echo 0b101001011,"<br>"; //base binária
+echo 0x117acf0,"<br>"; //base hexadecimal
