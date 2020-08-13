15a2a9bd946222332acf2a04c962dfb58ce2783f
Wanderson3004 <wandersonsousa3004@gmail.com>
2020-08-13 12:03:08 -0300

    assignments var complete

---
+++ variaveis/atribuicoes.php
@@ -0,0 +1,24 @@
+<div class="titulo">Atribuições</div>
+
+<?php
+
+$number =  1;
+$numberSum = 9;
+
+$numberSum += $number;
+
+var_dump( $numberSum );
+
+echo '<br>';
+
+# 
+# ?? equivale ao || do javascript
+
+$undefinedValue = null;
+unset($undefinedValue);
+$someValue = 99;
+
+$result = $undefinedValue ?? $someValue;
+
+
+var_dump($result);
