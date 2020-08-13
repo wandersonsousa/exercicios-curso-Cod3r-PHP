6ee5394d6d97452838ade7188b72c369a9cd155b
Wanderson3004 <wandersonsousa3004@gmail.com>
2020-08-13 12:26:25 -0300

    assignament var complete

--- index.php
+++ index.php
@@ -47,6 +47,8 @@
                         <li><a href="exercicio.php?dir=variaveis&file=basico">Básico</a></li>
                          <li><a href="exercicio.php?dir=variaveis&file=desafio_equacao">Desafio Equação</a></li>
                         <li><a href="exercicio.php?dir=variaveis&file=atribuicoes">Atribuições</a></li>
+                        <li><a href="exercicio.php?dir=variaveis&file=interpolacao">Interpolação</a></li>
+                        <li><a href="exercicio.php?dir=variaveis&file=variaveis_variaveis">Variáveis Variáveis</a></li>
                     </ul>       
                 </div>
                 <div class="modulo roxo">
---
+++ variaveis/interpolacao.php
@@ -0,0 +1,15 @@
+<div class="titulo">Interpolação</div>
+
+<?php
+
+
+$numero = 1998;
+
+echo "$numero <br>";
+
+
+echo '$numero <br>';
+
+echo "{$numero}990 <br>";
+
+
