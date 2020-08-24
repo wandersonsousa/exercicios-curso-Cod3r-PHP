fb22bfa99b3a3bafdbb672737a5b49592cee5502
Wanderson3004 <wandersonsousa3004@gmail.com>
2020-08-24 12:12:39 -0300

    trait one complete

--- classes_objetos/traits_01.php
+++ classes_objetos/traits_01.php
@@ -2,3 +2,32 @@
 
 <?php
 
+trait validacao {
+    public $valorA = 'A';
+    public function validarString($str){
+        return isset($str) && $str !== '';
+    }
+}
+
+trait validacaoMelhor {
+    public $valorB = 'B';
+    private $valorC = 'C';
+    public function validarStringMelhor($str){
+        return isset($str) && trim($str);
+    }
+    public function showC (){
+        echo $this -> valorC;
+    }
+}
+
+class Usuario {
+    use validacao, validacaoMelhor;
+}
+
+$usuario = new Usuario();
+
+var_dump($usuario -> validarStringMelhor(' ') );
+
+echo '<br>';
+
+$usuario -> showC();
