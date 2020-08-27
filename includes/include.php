d2f97bfe7920de5e7fa96a170ad39ac5233b041d
Wanderson3004 <wandersonsousa3004@gmail.com>
2020-08-26 23:44:20 -0300

    include the menu content apart

--- includes/include.php
+++ includes/include.php
@@ -2,9 +2,23 @@
 
 <?php
 
-include('include_arquivo.php');
-echo soma(1,9) . "<br>";
-echo "Valor Variável : $variavel";
+// include('include_arquivo.php');
+// echo soma(1,9) . "<br>";
+// echo "Valor Variável : $variavel";
 
-include('include_arquivo.php'); 
-echo soma(1,2) . "<br>";
+
+// function incluir(){
+//     include('include_arquivo.php'); 
+// }
+// for ($i=0; $i < 5; $i++) { 
+//     incluir();
+// }
+
+
+// include 'a.php';
+// echo "Continuou <br>";
+// require 'a.php';
+// echo "Não mostra essa string <br>";
+
+include_once 'include_arquivo.php';
+include_once 'include_arquivo.php'; 
---
+++ menu.php
@@ -0,0 +1,86 @@
+<div class="modulos">         
+    <div class="modulo verde">
+        <h3>Básico</h3>
+        <ul>
+            <li><a href="exercicio.php?dir=basico&file=ola">Olá PHP</a></li>
+            <li><a href="exercicio.php?dir=basico&file=html">Integração HTML</a></li>
+            <li><a href="exercicio.php?dir=basico&file=css">Integração CSS</a></li>
+            <li><a href="exercicio.php?dir=basico&file=comentarios">Comentários PHP</a></li>
+            <li><a href="exercicio.php?dir=basico&file=desafio">Desafio Básico</a></li>
+        </ul>
+    </div>
+    <div class="modulo vermelho">
+        <h3>Tipos</h3>
+        <ul>
+            <li><a href="exercicio.php?dir=tipos&file=int">Tipo Int</a></li>
+            <li><a href="exercicio.php?dir=tipos&file=float">Tipo Float</li>
+            <li><a href="exercicio.php?dir=tipos&file=aritimeticas">Op. Aritiméticas</a></li>
+            <li><a href="exercicio.php?dir=tipos&file=desafio_precedencia">Desafio Precedência</a></li>
+            <li><a href="exercicio.php?dir=tipos&file=string">Tipo String</a></li>
+            <li><a href="exercicio.php?dir=tipos&file=desafio_string">Desafio String</a></li>
+            <li><a href="exercicio.php?dir=tipos&file=boolean">Tipo Boolean</a></li>
+            <li><a href="exercicio.php?dir=tipos&file=conversoes">Conversões de Tipos</a></li>
+        </ul>
+    </div>
+    <div class="modulo azul">
+        <h3>Módulo 03 - Variáveis</h3>
+        <ul>
+            <li><a href="exercicio.php?dir=variaveis&file=basico">Básico</a></li>
+            <li><a href="exercicio.php?dir=variaveis&file=desafio_equacao">Desafio Equação</a></li>
+            <li><a href="exercicio.php?dir=variaveis&file=atribuicoes">Atribuições</a></li>
+            <li><a href="exercicio.php?dir=variaveis&file=interpolacao">Interpolação</a></li>
+            <li><a href="exercicio.php?dir=variaveis&file=variaveis_variaveis">Variáveis Variáveis</a></li>
+            <li><a href="exercicio.php?dir=variaveis&file=desafio_varvar">Desafio Variáveis Variáveis</a></li>
+            <li><a href="exercicio.php?dir=variaveis&file=valor_referencia">Valor vs Referência</a></li>
+            <li><a href="exercicio.php?dir=variaveis&file=constantes">Constantes</a></li>
+        </ul>
+    </div>
+    <div class="modulo roxo">
+        <h3>Módulo 04 - Estruturas de Controle</h3>
+        <ul>
+            <li><a href="exercicio.php?dir=controle&file=desafio_pi">Desafio PI</a></li>
+            <li><a href="exercicio.php?dir=controle&file=desafio_operadores">Desafio Operadores Lógicos</a></li>
+            <li><a href="exercicio.php?dir=controle&file=desafio_switch">Desafio Switch</a></li>
+        </ul>       
+    </div>
+    <div class="modulo laranja">
+        <h3>Módulo 05 - Estrutura de Controle II</h3>
+        <ul>
+        <li><a href="exercicio.php?dir=controle2&file=for">FOR</a></li>
+        </ul>       
+    </div>
+    <div class="modulo verde-escuro">
+        <h3>Módulo 06 - Orientação A Objetos</h3>
+        <ul>
+            <li><a href="exercicio.php?dir=classes_objetos&file=visibilidade">Visibilidade</a></li>
+            <li><a href="exercicio.php?dir=classes_objetos&file=static">Membros Estáticos</a></li>
+            <li><a href="exercicio.php?dir=classes_objetos&file=desafio_erros">Desafio Erros</a></li>
+            <li><a href="exercicio.php?dir=classes_objetos&file=final">Final</a></li>
+            <li><a href="exercicio.php?dir=classes_objetos&file=traits_01">Traits 01</a></li>
+            <li><a href="exercicio.php?dir=classes_objetos&file=polimorfismo">Polimorfismo</a></li>
+        </ul>       
+    </div>
+
+    <div class="modulo vermelho-escuro">
+        <h3>Módulo 07 - Includes</h3>
+        <ul>
+            <li><a href="exercicio.php?dir=includes&file=include">Include</a></li>
+        </ul>       
+    </div>
+    <div class="modulo azul-escuro">
+        <h3>Módulo 08 - Namespaces</h3>
+        <ul>
+            <li><a href="exercicio.php?dir=namespace&file=basico">Básico</a></li>
+            <li><a href="#">Exercício B</a></li>
+            <li><a href="#">Exercício C</a></li>
+        </ul>       
+    </div>
+    <div class="modulo roxo-escuro">
+        <h3>Módulo 09 - Básico</h3>
+        <ul>
+            <li><a href="#">Exercício A</a></li>
+            <li><a href="#">Exercício B</a></li>
+            <li><a href="#">Exercício C</a></li>
+        </ul>       
+    </div>           
+</div>
